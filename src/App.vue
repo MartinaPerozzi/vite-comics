d7fb62ca3c350c7e95c23e2ba041b522a5234d1e
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-27 15:33:48 +0100

    structure

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: { AppHeader, }
+};
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;600;900&display=swap');
+</style>
